244f264f77124a6147e1b36c5713558eb5219f4e
VigularIgnat <ignatvigular@gmail.com>
2023-08-30 21:47:34 +0200

    Fixing course changing trouble

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -18,60 +18,37 @@ class CurrencyService{
     public function updatecourse(){
         $url=$this->api_endpoint.$this->token.'&currencies=PLN%2CUAH%2CEUR%2CCZK%2CDKK%2CGBP';
         try{
-            //$response=Http::get($url);
-            $body_currencies='{
-                "meta": {
-                  "last_updated_at": "2023-08-28T23:59:59Z"
-                },
-                "data": {
-                  "CZK": {
-                    "code": "CZK",
-                    "value": 22.2967630806
-                  },
-                  "DKK": {
-                    "code": "DKK",
-                    "value": 6.8823608651
-                  },
-                  "EUR": {
-                    "code": "EUR",
-                    "value": 0.9235201142
-                  },
-                  "GBP": {
-                    "code": "GBP",
-                    "value": 0.7928301095
-                  },
-                  "PLN": {
-                    "code": "PLN",
-                    "value": 4.1250105174
-                  },
-                  "UAH": {
-                    "code": "UAH",
-                    "value": 36.6810470021
-                  }
-                }
-              }';
+            $response=Http::get($url);
+           // dd($response->json());
+            //$body_currencies=$response->json();
             //$response->json();
             //https://api.currencyapi.com/v3/currencies?apikey=cur_live_81BYHyLjG95GhLsgqRtkRcJZ7UclRdRagHC51NAw&currencies=PLN%2CUAH%2CEUR%2CCZK%2CDKK%2CGBP
             //return $body_currencies;
-            $data_decoded=json_decode($body_currencies, true);
+            //dd($body_currencies);
+            $data_decoded=json_decode($response, true);
+            
             $data_course=$data_decoded['data'];
-
+            
             $array_courses=[];
             foreach ($this->currencies as $currency) {
                 foreach ($data_course as $data_cur) {
                     
                     //echo 'currency '.$currency."data_cur ".$data_cur['code'];
                     if($currency==$data_cur['code']){
-                        
-                        $currency_el=Currency::where('name',$currency)->get();
-                        
+                      
+                        $currency_el=Currency::where('name',$currency)->first();
+                        //dd($currency_el);
                         $currency_el->course=round($data_cur['value'],2);
+                        //dd(round($data_cur['value'],2));
+                        //return response()->json($data_cur['value'],2);
                         $array_courses[$currency]=round($data_cur['value'],2);
-                        return response()->json($data_cur['value'],2);
+                        
                         $currency_el->save();
+                        
                     }
                 }
             }
+            //dd($array_courses);
             //return response()->json($data_course);
             //https://api.currencyapi.com/v3/latest?apikey=cur_live_81BYHyLjG95GhLsgqRtkRcJZ7UclRdRagHC51NAw&currencies=PLN%2CUAH%2CEUR%2CUSD%2CCZK%2CDKK
             //https://api.currencyapi.com/v3/latest?apikey=cur_live_81BYHyLjG95GhLsgqRtkRcJZ7UclRdRagHC51NAw&currencies=EUR%2CUSD%2CCZK%2CPLN%2CUAH
